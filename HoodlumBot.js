0a8fed888eedfbf2dc2c3818ac54c5a444cd2167
fancyhoodlum <38635423+fancyhoodlum@users.noreply.github.com>
2018-04-22 23:37:46 +0100

    Update HoodlumBot.js

--- HoodlumBot.js
+++ HoodlumBot.js
@@ -1,11 +1,16 @@
 {
-  "name": "HoodlumBot",
-  "version": "1.0.0",
+  "name": "Hoodlumbot",
+  "version": "1.0.1",
   "description": "My own Discord bot",
   "main": "bot.js",
   "author": "FANCYHOODLUM",
   "dependencies": {}
 }
+
+{
+   "token": "NDM3NzM4NTUxMDc4Mjg5NDE5.Db6fFw.2lMGaZbU0cBj1tasm0RMWH3rIf0"
+}
+
 
 var Discord = require('discord.io');
 var logger = require('winston');
@@ -27,8 +32,7 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+   
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -45,144 +49,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             // Just add any case commands if you want to..
          }
      }
-});node bot.js
-
-
-// !ping 
-case 'ping' 
-bot.sendmessage({
-    to: channelID
-    message: 'Pong!'
 });
-break;
+view rawbot.js hosted with ❤ by GitHub
 
+npm install discord.io winston --save
 
-})var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-     `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});{
-  
-  string token = "NDM3NzM4NTUxMDc4Mjg5NDE5.Db6bjQ.P_JT_FRPChd-2LL3Tn6E6PiC4R8";
-await _client.LoginAsync(TokenType.Bot, token);
-await _client.StartAsync();
-}
-def compute_base_permissions(member, guild):
-    if guild.is_owner(member):
-        return ALL
-
-    role_everyone = guild.get_role(guild.id)  # get @everyone role
-    permissions = role_everyone.permissions
-
-    for role in member.roles:
-        permissions |= role.permissions
-
-    if permissions & ADMINISTRATOR == ADMINISTRATOR:
-        return ALL
-
-def compute_overwrites(base_permissions, member, channel):
-    # ADMINISTRATOR overrides any potential permission overwrites, so there is nothing to do here.
-    if base_permissions & ADMINISTRATOR == ADMINISTRATOR:
-        return ALL
-
-    permissions = base_permissions
-    overwrite_everyone = overwrites.get(channel.guild_id)  # Find (@everyone) role overwrite and apply it.
-    if overwrite_everyone:
-        permissions &= ~overwrite_everyone.deny
-        permissions |= overwrite_everyone.allow
-
-    # Apply role specific overwrites.
-    overwrites = channel.permission_overwrites
-    allow = NONE
-    deny = NONE
-    for role_id in member.roles:
-        overwrite_role = overwrites.get(role_id)
-        if overwrite_role:
-            allow |= overwrite_role.allow
-            deny |= overwrite_role.deny
-
-    permissions &= ~deny
-    permissions |= allow
-
-    # Apply member specific overwrite if it exist.
-    overwrite_member = overwrites.get(member.user_id)
-    if overwrite_member:
-        permissions &= ~overwrite_member.deny
-        permissions |= overwrite_member.allow
-
-    return permissions
-
-def compute_permissions(member, channel):
-    base_permissions = compute_base_permissions(member, channel.guild)
-    return compute_overwrites(base_permissions, member, channel)
-{
-    discord.ExecuteAndWait(async () =>
-    {
-        await discord.Connect("TNDM3NzM4NTUxMDc4Mjg5NDE5.Db6bjQ.P_JT_FRPChd-2LL3Tn6E6PiC4R8
-", TokenType.Bot);   
-    });
-}
-catch
-{
-
-}
-string token = "NDM3NzM4NTUxMDc4Mjg5NDE5.Db6bjQ.P_JT_FRPChd-2LL3Tn6E6PiC4R8");
-await _client.LoginAsync(TokenType.Bot, NDM3NzM4NTUxMDc4Mjg5NDE5.Db6bjQ.P_JT_FRPChd-2LL3Tn6E6PiC4R8);
-await _client.StartAsync();
-
-  
-    "id": "437738551078289419",
-    "username": "Hoodlumbot",
-    "discriminator": "1337",
-  
-    "verified": true,
-      
-      def exchange_code(code):
-  data = {
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-    'grant_type': 'authorization_code',
-    'code': code,
-    'redirect_uri': REDIRECT_URI
-  }
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-  r = requests.post('%s/oauth2/token' % API_ENDPOINT, data, headers)
-  r.raise_for_status()
-  return r.json()
-    
 node bot.js
